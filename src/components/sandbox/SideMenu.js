3469ee45e39cf76c014ab3d5ab16341fb5584182
pollylilh <794211268@qq.com>
2022-06-02 16:38:39 +0800

    first commit

---
+++ src/components/sandbox/SideMenu.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Layout, Menu } from 'antd';
+import './index.css'
+import {withRouter} from 'react-router-dom'
+import { UserOutlined } from '@ant-design/icons';
+const { Sider } = Layout;
+const { SubMenu } = Menu
+
+//模拟数组结构
+const  menuList = [
+  {
+    key:"/home",
+    title:"首页",
+    icon:<UserOutlined />
+  },
+  {
+    key:"/user-manage",
+    title:"用户管理",
+    icon:<UserOutlined />,
+    children:[
+      {
+        key:"/user-manage/list",
+        title:"用户列表",
+        icon:<UserOutlined />
+      }
+    ]
+  },
+  {
+    key:"/right-manage",
+    title:"权限管理",
+    icon:<UserOutlined />,
+    children:[
+      {
+        key:"/right-manage/role/list",
+        title:"角色列表",
+        icon:<UserOutlined />
+      },
+      {
+        key:"/right-manage/right/list",
+        title:"权限列表",
+        icon:<UserOutlined />
+      }
+    ]
+  }
+]
+
+function SideMenu(props) {
+
+  const renderMenu = (menuList)=>{
+    return menuList.map(item=>{
+      if(item.children){
+        return <SubMenu key={item.key} icon={item.icon} title={item.title}>
+           { renderMenu(item.children) }
+        </SubMenu>
+      }
+
+      return <Menu.Item key={item.key} icon={item.icon} onClick={()=>{
+        props.history.push(item.key)
+      }}>{item.title}</Menu.Item>
+    })
+  }
+  return (
+    <Sider trigger={null} collapsible collapsed={false}>
+      <div className="logo" >后台管理系统</div>
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['3']}>
+          {renderMenu(menuList)}
+      </Menu>
+    </Sider>
+  )
+}
+export default withRouter(SideMenu)
